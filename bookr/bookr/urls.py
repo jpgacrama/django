f3b6333f9ba8c0fbf1c3752acde8aff485a77f7f
Jonas Gacrama <jonasgacrama@gmail.com>
2023-07-24 16:34:46 +0800

    main: Search template is now created

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -20,5 +21,6 @@ import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index)
+    path('', reviews.views.index),
+    path('book-search/', reviews.views.search, name='book_search'),
 ]
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "base.html")
+
+def search(request):
+    search_query = request.GET.get('search', '')  # Get the search string from the URL parameters
+    context = {
+        'search_query': search_query,
+    }
+    return render(request, "search.html", context)
